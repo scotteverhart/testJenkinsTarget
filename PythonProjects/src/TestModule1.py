a9c172dcc83fff4343c453740f43a7f370257393
cs_everhart <scott.everhart1@gmail.com>
2017-03-11 23:36:14 -0500

    Adding python files

---
+++ PythonProjects/src/TestModule1.py
@@ -0,0 +1,33 @@
+"""
+@created:   Feb 20, 2017
+@author: scotteverhart
+"""
+
+import stringFormatter
+import random
+
+
+class printStuff():
+
+    randNum = random.randint(1,10)
+    num = range(0,randNum)
+    listValues = ["one  ","two  ","three","four ","five ", "six ", "seven ", "eight ", "nine ", "ten ", "eleven "]
+    
+    print
+    print "randNum: ",randNum
+    print "Values in num: ",num
+    print "Values in listValues: ",listValues
+    print
+        
+    def printRange(self):
+        for i in self.num:
+            print i,self.printText(i-1)
+        print "class processing complete"
+            
+    def printText(self,listNum):
+        return "\t" + stringFormatter.addTwoHyphensBeforeAndAfter(self.listValues[listNum]) + "\t" + \
+            stringFormatter.addTwoAsterisksBeforeAndAfter(self.listValues[listNum]) + "\t" + \
+            stringFormatter.addTwoCaretsBeforeAndAfter(self.listValues[listNum])
+        
+if __name__ == '__main__':
+    printStuff().printRange()
