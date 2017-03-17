dfa1985d6e2cf86522c8eeeb71a3d3a07d21c3a9
cs_everhart <scott.everhart1@gmail.com>
2017-03-16 23:41:07 -0400

    update to file write

--- PythonProjects/src/TestModule1.py
+++ PythonProjects/src/TestModule1.py
@@ -31,7 +31,7 @@ class printStuff():
             stringFormatter.addTwoAsterisksBeforeAndAfter(self.listValues[listNum]) + "\t" + \
             stringFormatter.addTwoCaretsBeforeAndAfter(self.listValues[listNum])
             
-        self.of =file(outputFileName, 'w')
+        self.of =file(self.outputFileName, 'w')
         self.of.write(outputtext)
         
 if __name__ == '__main__':
